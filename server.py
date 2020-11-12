a6294b5390c764f4b3eb7efca20d0432af82317e
janepavlova <pavlova.jane@gmail.com>
2020-11-12 15:28:36 +0100

    Add client UI

--- server.py
+++ server.py
@@ -6,31 +6,65 @@ app = Flask(__name__)
 
 import time
 
-def current_time():
+db = [
+    {
+        'text':'Hi',
+        'name':'Vasya',
+        'time':time.time()
+    },
+    {
+        'text':'Hi Vasya',
+        'name':'Kolya',
+        'time':time.time()
+    },
+    {
+        'text':'Howdy?',
+        'name':'Vasya',
+        'time':time.time()
+    }
+]
+
+def show_time():
     t = time.localtime()
-    current_time = time.strftime("%H:%M:%S", t)
-    return current_time
+    local_time = time.strftime("%H:%M:%S", t)
+    return local_time
 
+@app.route("/send")
+def send_message(text, name):
+    db.append({
+        'text': text,
+        'name': name,
+        'time': time.time()
+    })
 
+@app.route("/messages")
+def get_messages(after):
+    filtered_db = []
+    for message in db:
+        if message['time'] > after:
+            filtered_db.append(message)
+    return filtered_db
 
 @app.route('/static/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+
 @app.route("/")
 def hello():
     return render_template('index.html')
 
+
 status = {
         'status': True,
         'name': 'Hallochen',
-        'time': current_time()
+        'time': show_time()
             }
+
 
 @app.route("/stats")
 def stats():
     return jsonify(status)
-
 
 app.run()
